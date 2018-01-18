6f91b19f839f9b1970fc3b520b978f04712ddc70
aab1986 <aab.1986@live.com>
2018-01-17 22:48:04 -0600

    Update and rename mgos_mylib.c to mgos_empty.c

--- src/mgos_empty.c
+++ src/mgos_empty.c
@@ -1,7 +1,7 @@
-#include "mgos_mylib.h"
+#include "mgos_empty.h"
 
 // NOTE: library init function must be called mgos_LIBNAME_init()
-bool mgos_mylib_init(void) {
+bool mgos_empty_init(void) {
   return true;
 }
 
